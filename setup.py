9f7b161e76afa649b3f7504c4f289d153ab27871
joe <joe.maliksi@gmail.com>
2020-08-30 14:38:25 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+with open('README.md', 'r') as f:
+    long_desc = f.read()
+
+
+setuptools.setup(
+    name='blaseball-mike',
+    version='0.0.1',
+    author='Joe Maliksi',
+    author_email='joe.maliksi@gmail.com',
+    url='https://github.com/jmaliksi/blaseball-mike',
+    description='Read-only Python wrapper around blaseball game API'
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+)
