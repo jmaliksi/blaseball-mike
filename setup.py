a318ee6e4d9c0c70dad741861f00f48fed9dc953
joe <joe.maliksi@gmail.com>
2020-08-30 14:52:48 -0400

    typo

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     author='Joe Maliksi',
     author_email='joe.maliksi@gmail.com',
     url='https://github.com/jmaliksi/blaseball-mike',
-    description='Read-only Python wrapper around blaseball game API'
+    description='Read-only Python wrapper around blaseball game API',
     long_description=long_desc,
     long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
