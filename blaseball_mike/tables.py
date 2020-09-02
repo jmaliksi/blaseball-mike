c4e1362332b7790c8bc346c233857430fb52a0e0
Ryan Littleton <ryan.littleton0@gmail.com>
2020-09-02 08:58:50 -0400

    Clean up tables

--- blaseball_mike/tables.py
+++ blaseball_mike/tables.py
@@ -1,113 +1,69 @@
-import enum
+class Weather:
+    _weathers = {
+        0: "Void",
+        1: "Sunny",
+        2: "Overcast",
+        3: "Rainy",
+        4: "Sandstorm",
+        5: "Snowy",
+        6: "Acidic",
+        7: "Solar Eclipse",
+        8: "Glitter",
+        9: "Bloodwind",
+        10: "Peanuts",
+        11: "Birds",
+        12: "Feedback",
+        13: "Reverb"
+    }
+
+    def __init__(self, value):
+        if value not in self._weathers:
+            self.value = "Invalid Weather"
+        self.value = self._weathers[value]
 
 
-class Weather(enum.IntEnum):
-    VOID = 0
-    SUNNY = 1
-    OVERCAST = 2
-    RAINY = 3
-    SANDSTORM = 4
-    SNOWY = 5
-    ACIDIC = 6
-    SOLAR_ECLIPSE = 7
-    GLITTER = 8
-    BLOODWIND = 9
-    PEANUTS = 10
-    BIRD = 11
-    FEEDBACK = 12
-    REVERB = 13
+class Blood:
+    _bloods = {
+        0: "A",
+        1: "AAA",
+        2: "AA",
+        3: "Acidic",
+        4: "Basic",
+        5: "O",
+        6: "O No",
+        7: "H\u2082O",
+        8: "Electric",
+        9: "Love",
+        10: "Fire",
+        11: "Psychic",
+        12: "Grass"
+    }
+
+    def __init__(self, value):
+        if value not in self._bloods:
+            self.value = "Blood?"
+        self.value = self._bloods[value]
 
 
-class Blood(enum.IntEnum):
-    SINGLE_A = 0
-    TRIPLE_A = 1
-    DOUBLE_A = 2
-    ACID = 3
-    BASE = 4
-    OH = 5
-    OH_NO = 6
-    WATER = 7
-    ELECTRIC = 8
-    LOVE = 9
-    FIRE = 10
-    PSYCHIC = 11
-    GRASS = 12
+class Coffee:
+    _coffees = {
+        0: "Black",
+        1: "Light & Sweet",
+        2: "Macchiato",
+        3: "Cream & Sugar",
+        4: "Cold Brew",
+        5: "Flat White",
+        6: "Americano",
+        7: "Coffee?",  # Note: Should be "Expresso", but bugged in site code
+        8: "Heavy Foam",
+        9: "Latte",
+        10: "Decaf",
+        11: "Milk Substitute",
+        12: "Plenty of Sugar",
+        13: "Anything"
+    }
 
-    def __str__(self):
-        if self.value == Blood.SINGLE_A:
-            return "A"
-        elif self.value == Blood.DOUBLE_A:
-            return "AA"
-        elif self.value == Blood.TRIPLE_A:
-            return "AAA"
-        elif self.value == Blood.ACID:
-            return "Acidic"
-        elif self.value == Blood.BASE:
-            return "Basic"
-        elif self.value == Blood.OH:
-            return "O"
-        elif self.value == Blood.OH_NO:
-            return "O No"
-        elif self.value == Blood.WATER:
-            return "H\u2082O"
-        elif self.value == Blood.ELECTRIC:
-            return "Electric"
-        elif self.value == Blood.LOVE:
-            return "Love"
-        elif self.value == Blood.FIRE:
-            return "Fire"
-        elif self.value == Blood.PSYCHIC:
-            return "Psychic"
-        elif self.value == Blood.GRASS:
-            return "Grass"
-        else:
-            return "Blood?"
-
-
-class Coffee(enum.IntEnum):
-    BLACK = 0
-    LIGHT_AND_SWEET = 1
-    MACCHIATO = 2
-    CREAM_AND_SUGAR = 3
-    COLD_BREW = 4
-    FLAT_WHITE = 5
-    AMERICANO = 6
-    EXPRESSO = 7
-    FOAM = 8
-    LATTE = 9
-    DECAF = 10
-    MILK_SUBSTITUTE = 11
-    PLENTY_OF_SUGAR = 12
-    ANYTHING = 13
-
-    def __str__(self):
-        if self.value == Coffee.BLACK:
-            return "Black"
-        elif self.value == Coffee.LIGHT_AND_SWEET:
-            return "Light & Sweet"
-        elif self.value == Coffee.MACCHIATO:
-            return "Macchiato"
-        elif self.value == Coffee.CREAM_AND_SUGAR:
-            return "Cream & Sugar"
-        elif self.value == Coffee.COLD_BREW:
-            return "Cold Brew"
-        elif self.value == Coffee.FLAT_WHITE:
-            return "Flat White"
-        elif self.value == Coffee.AMERICANO:
-            return "Americano"
-        elif self.value == Coffee.EXPRESSO:
-            return "Coffee?"  # This is bugged on the main site, so recreate bug here
-        elif self.value == Coffee.FOAM:
-            return "Heavy Foam"
-        elif self.value == Coffee.LATTE:
-            return "Latte"
-        elif self.value == Coffee.DECAF:
-            return "Decaf"
-        elif self.value == Coffee.MILK_SUBSTITUTE:
-            return "Milk Substitute"
-        elif self.value == Coffee.PLENTY_OF_SUGAR:
-            return "Plenty of Sugar"
-        elif self.value == Coffee.ANYTHING:
-            return "Anything"
-        else:
-            return "Coffee?"
+    def __init__(self, value):
+        if value not in self._coffees:
+            self.value = "Coffee?"
+        self.value = self._coffees[value]
