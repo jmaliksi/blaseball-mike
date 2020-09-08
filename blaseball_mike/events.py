8f4c8af323169d36b2c5f8daf9f44c64da62c8e4
joe <joe.maliksi@gmail.com>
2020-09-07 21:09:21 -0400

    fix event error handling

--- blaseball_mike/events.py
+++ blaseball_mike/events.py
@@ -2,13 +2,14 @@
 Wrapper around the SSE events API.
 """
 import asyncio
+from concurrent import futures
 import ujson
 
 from aiohttp_sse_client import client as sse_client
 from aiohttp.client_exceptions import ClientPayloadError
 
 
-async def stream_events(retry_base=0.1, retry_max=300):
+async def stream_events(retry_base=0.01, retry_max=300):
     """
     Async generator for the events API.
     `retry_base` will be the minimum time to delay if there's a connection error
@@ -19,11 +20,11 @@ async def stream_events(retry_base=0.1, retry_max=300):
         try:
             async with sse_client.EventSource('https://www.blaseball.com/events/streamData') as src:
                 async for event in src:
-                    retry_delay = 0.1  # reset backoff
+                    retry_delay = retry_base  # reset backoff
                     if not event.data:
                         continue
                     payload = ujson.loads(event.data)['value']
                     yield payload
-        except (ConnectionError, TimeoutError, ClientPayloadError):
+        except (ConnectionError, TimeoutError, ClientPayloadError, futures.TimeoutError):
             await asyncio.sleep(retry_delay)
             retry_delay = min(retry_delay * 2, retry_max)
