6ef4a69dd25f41b4a940847107ad35a7ee0e353b
Joe Maliksi <jmaliksi@users.noreply.github.com>
2020-09-20 20:35:00 -0400

    1.8.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='blaseball-mike',
-    version='1.8.0',
+    version='1.8.1',
     author='Joe Maliksi',
     author_email='joe.maliksi@gmail.com',
     url='https://github.com/jmaliksi/blaseball-mike',
