e21220add9ca00be92272e8fbbf7b6df015406e6
Ryan Littleton <ryan.littleton0@gmail.com>
2020-10-25 16:38:07 -0400

    Add Tarot cards

--- blaseball_mike/tables.py
+++ blaseball_mike/tables.py
@@ -91,6 +91,42 @@ class Coffee(Enum):
         return obj
 
 
+class Tarot(Enum):
+    INVALID = -1, "----"
+    MAGICIAN = 0, "I The Magician"
+    HIGH_PRIESTESS = 1, "II The High Priestess"
+    EMPRESS = 2, "III The Empress"
+    EMPEROR = 3, "IIII The Emperor"
+    POPE = 4, "V The Pope"
+    LOVER = 5, "VI The Lover"
+    CHARIOT = 6, "VII The Chariot"
+    JUSTICE = 7, "VIII Justice"
+    HERMIT = 8, "VIIII The Hermit"
+    WHEEL_OF_FORTUNE = 9, "X The Wheel of Fortune"
+    STRENGTH = 10, "XI Strength"
+    HANGED_MAN = 11, "XII The Hanged Man"
+    DEATH = 12, "XIII"
+    TEMPERANCE = 13, "XIIII Temperance"
+    DEVIL = 14, "XV The Devil"
+    TOWER = 15, "XVI The Tower"
+    STAR = 16, "XVII The Star"
+    MOON = 17, "XVIII The Moon"
+    SUN = 18, "XVIIII The Sun"
+    JUDGMENT = 19, "XX Judgment"
+
+    @classmethod
+    def _missing_(cls, value):
+        t = cls.INVALID
+        t._value_ = value
+        return t
+
+    def __new__(cls, keycode, text):
+        obj = object.__new__(cls)
+        obj._value_ = keycode
+        obj.text = text
+        return obj
+
+
 class Modification(Enum):
     INVALID = None, "????", "This Modification is unknown."
     EXTRA_STRIKE = "EXTRA_STRIKE", "The Fourth Strike", \
