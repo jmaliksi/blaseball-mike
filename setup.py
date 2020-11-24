7df5d542dbd69ffd0a2b85b53d312dc7ad1be3e9
Joe Maliksi <jmaliksi@users.noreply.github.com>
2020-11-24 08:41:42 -0500

    4.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_requires = [
 
 setuptools.setup(
     name='blaseball-mike',
-    version='4.0.1',
+    version='4.1.0',
     author='Joe Maliksi',
     author_email='joe.maliksi@gmail.com',
     url='https://github.com/jmaliksi/blaseball-mike',
