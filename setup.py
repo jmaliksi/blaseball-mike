31afa234674591feaadac86fca085872143fd4af
Ryan Littleton <ryan.littleton0@gmail.com>
2021-04-10 17:29:11 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
 
 setuptools.setup(
     name='blaseball-mike',
-    version='5.9.2',
+    version='5.10.0',
     author='Joe Maliksi',
     author_email='joe.maliksi@gmail.com',
     url='https://github.com/jmaliksi/blaseball-mike',
