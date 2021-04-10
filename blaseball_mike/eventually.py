89283352da1b04dde367ce8b83b3f77a1f65000d
allie <allie@yuri.cafe>
2021-04-10 16:22:24 -0300

    generator by default

--- blaseball_mike/eventually.py
+++ blaseball_mike/eventually.py
@@ -4,31 +4,9 @@ BASE_URL = 'https://api.sibr.dev/eventually'
 
 """
 Search through feed events.
-Set to limit -1 to get everything. (warning: may take a while!)
-Possible parameters for query: https://allie-signet.stoplight.io/docs/eventually/reference/eventually-api.v1.yaml/paths/~1events/get
-"""
-def search(cache_time=5, limit=100, query={}):
-    s = session(cache_time)
-
-    res = []
-    res_len = 0
-
-    while limit == -1 or res_len < limit:
-        out = check_network_response(s.get(f"{BASE_URL}/events",params={'offset': res_len, 'limit': 100, **query}))
-        out_len = len(out)
-        if out_len < 100:
-            res += out
-            break
-        else:
-            res_len += out_len
-            res += out
-
-    return res
-
-"""
-Search through feed events.
-Set to limit -1 to get everything. (warning: may take a while!)
+Set to limit -1 to get everything.
 Returns a generator that only gets the following page when needed.
+Possible parameters for query: https://allie-signet.stoplight.io/docs/eventually/reference/eventually-api.v1.yaml/paths/~1events/get
 """
 def lazy_search(cache_time=5, limit=100, query={}):
     s = session(cache_time)
