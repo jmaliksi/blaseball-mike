2201715f9e36b81e2a289e2c27a063c7fa55b021
Ryan Littleton <ryan.littleton0@gmail.com>
2021-05-25 17:12:12 -0400

    Move make_docs.py
    Gives us a directory to use for storing misc documentation

--- docs/make_docs.py
+++ docs/make_docs.py
@@ -1,10 +1,10 @@
 import pdoc
 import os
 import os.path as path
-import re
 from blaseball_mike.models.base import Base
 
-output_dir = 'docs'
+# Output directory, relative to the location of this script
+output_dir = '_build'
 
 
 def recurse_modules(mod):
@@ -14,7 +14,8 @@ def recurse_modules(mod):
 
 
 def recursive_write_files(m):
-    filepath = path.join(output_dir, m.url())
+    self_path = os.path.dirname(os.path.realpath(__file__))
+    filepath = path.join(self_path, output_dir, m.url())
 
     dirpath = path.dirname(filepath)
     if not os.access(dirpath, os.R_OK):
@@ -42,8 +43,6 @@ for module in recurse_modules(mike_module):
             continue
 
         for field in fields:
-            if field in class_.doc:
-                continue
             class_.doc[field] = pdoc.Variable(field, module, "", cls=class_, instance_var=True)
 
 # Write to files
