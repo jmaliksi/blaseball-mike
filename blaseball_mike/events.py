ae21dad244d729720125a31469fb1ff1ad424d26
Anthony <alefeld@alumni.nd.edu>
2021-11-10 20:43:18 -0500

    Update streamData for Gamma -Now saves a dict object that is modified when delta event comes through. -Handles most deltas just fine -Only known issue is sometimes a set of deltas come through that don't match LHS to the current state of the event. When this happens, that set of deltas is skipped (and so will the future deltas until a brand new event is sent).

--- blaseball_mike/events.py
+++ blaseball_mike/events.py
@@ -3,6 +3,7 @@ Wrapper around the SSE events API.
 """
 import asyncio
 from concurrent import futures
+import jsonpointer
 import ujson
 
 from aiohttp_sse_client import client as sse_client
@@ -16,14 +17,62 @@ async def stream_events(url='https://api.blaseball.com/events/streamData', retry
     `retry_max` is the maximum time to delay if there's a connection error
     """
     retry_delay = retry_base
+    event_tmp = {}
     while True:
         try:
-            async with sse_client.EventSource(url, read_bufsize=2 ** 18) as src:
+            async with sse_client.EventSource(url, read_bufsize=2 ** 19) as src:
                 async for event in src:
                     retry_delay = retry_base  # reset backoff
                     if not event.data:
                         continue
-                    payload = ujson.loads(event.data)['value']
+                    raw_event = ujson.loads(event.data)
+                    if 'value' in raw_event.keys():
+                        payload = raw_event['value']
+                        event_tmp = payload
+                    else: # If this is a delta (modification) of a past event, modify event_tmp.
+                        for delta in raw_event['delta']:
+                            path = '/'+'/'.join([str(item) for item in delta['path']])
+                            if delta['kind'] == 'E': # Replace a full dict element
+                                if 'lhs' in delta:
+                                    if delta['lhs'] == jsonpointer.resolve_pointer(event_tmp, path):
+                                        jsonpointer.set_pointer(event_tmp, path, delta['rhs'])
+                                    else:
+                                        print("Error. LHS does not match event_tmp!")
+                                        break
+                            elif delta['kind'] == 'A': # Modify a list
+                                if delta['item']['kind'] == 'D': # Delete a list element
+                                    thislist = jsonpointer.resolve_pointer(event_tmp, path)
+                                    if thislist[delta['index']] == delta['item']['lhs']:
+                                        del thislist[delta['index']]
+                                        jsonpointer.set_pointer(event_tmp, path, thislist)
+                                    else:
+                                        print("Error. LHS does not match event_tmp!")
+                                        break
+                                elif delta['item']['kind'] == 'N': # Add a new list element
+                                    thislist = jsonpointer.resolve_pointer(event_tmp, path)
+                                    thislist.insert(delta['index'], delta['item']['rhs'])
+                                    jsonpointer.set_pointer(event_tmp, path, thislist)
+                                else:
+                                    print("Unknown delta A item kind code!!")
+                                    print(delta)
+                            elif delta['kind'] == 'N': # Add a new element to a dict
+                                path,newkey = path.rsplit('/',1)
+                                thisdict = jsonpointer.resolve_pointer(event_tmp, path)
+                                thisdict[newkey] = delta['rhs']
+                                jsonpointer.set_pointer(event_tmp, path, thisdict)
+                            elif delta['kind'] == 'D': # Delete a dict element
+                                path,oldkey = path.rsplit('/',1)
+                                thisdict = jsonpointer.resolve_pointer(event_tmp, path)
+                                if thisdict[oldkey] == delta['lhs']:
+                                    del thisdict[oldkey]
+                                    jsonpointer.set_pointer(event_tmp, path, thisdict)
+                                else:
+                                    print("Error. LHS does not match event_tmp!")
+                                    break
+                            else:
+                                print("Unknown delta kind code!!")
+                                print(delta)
+                        payload = event_tmp
                     yield payload
         except (ConnectionError,
                 TimeoutError,
